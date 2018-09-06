49930d7e243a5abba14c29df456ed87d4ec243e2
Juan Sebastian Gil Garnica <Jgilg@intergrupo.com>
2018-09-06 10:40:00 -0500

    Add jwt

--- AgenciaDeEmpleoVirutal.Services/Controllers/CallHistoryTraceController.cs
+++ AgenciaDeEmpleoVirutal.Services/Controllers/CallHistoryTraceController.cs
@@ -9,7 +9,7 @@
     [Produces("application/json")]
     [Route("api/Agent")]
     [EnableCors("CorsPolitic")]
-    [Authorize]
+    ///[Authorize]
     public class CallHistoryTraceController : Controller
     {
         private ICallHistoryTrace _callHistoryBusiness;
