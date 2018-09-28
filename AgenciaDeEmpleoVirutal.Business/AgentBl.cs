c3eaf512949083930288a77d794f9aa67f223173
Juan Sebastian Gil Garnica <Jgilg@intergrupo.com>
2018-09-28 14:17:07 -0500

    modificacion en obtener agentes disponibles

--- AgenciaDeEmpleoVirutal.Business/AgentBl.cs
+++ AgenciaDeEmpleoVirutal.Business/AgentBl.cs
@@ -84,15 +84,9 @@
                 };
 
                 var advisors = _agentRepository.GetSomeAsync(query).Result;
-
-                /// var advisors = _agentRepository.GetByPatitionKeyAsync(UsersTypes.Funcionario.ToString().ToLower()).Result;
                 if (advisors.Count.Equals(0))
                     return ResponseFail<GetAgentAvailableResponse>(ServiceResponseCode.AgentNotFound);
-                ///var Agent = advisors.Where(i => i.Available ).OrderBy(x => x.CountCallAttended).FirstOrDefault();
                 var Agent = advisors.OrderBy(x => x.CountCallAttended).FirstOrDefault();
-
-                if (Agent == null)
-                    return ResponseFail<GetAgentAvailableResponse>(ServiceResponseCode.AgentNotAvailable);
 
                 //Disabled Agent
                 Agent.Available = false;
