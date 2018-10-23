fcfed16a8a36d35a2e5fb52d75973215b30ae4c1
Juan Sebastian Gil Garnica <Jgilg@intergrupo.com>
2018-10-23 17:01:18 -0500

    Ajuste Sonar

--- AgenciaDeEmpleoVirutal.Business/ResetBI.cs
+++ AgenciaDeEmpleoVirutal.Business/ResetBI.cs
@@ -56,7 +56,7 @@
                 state = UsersTypes.Empresa.ToString();
                 userAux = userAux.Replace("_empresa", "");
             }
-            else /* if (userAux.IndexOf("_funcionario") > -1) */
+            else
             {
                 state = UsersTypes.Funcionario.ToString();
                 userAux = userAux.Replace("_funcionario", "");
