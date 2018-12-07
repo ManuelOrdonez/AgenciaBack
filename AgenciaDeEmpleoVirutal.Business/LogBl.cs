80bce2c257a5d5ec4df8174f24feb0137f96ffe4
Manuel Alexander Ordonez Olaya <Mordonez@intergrupo.com>
2018-12-07 10:16:08 -0500

    Logs

---
+++ AgenciaDeEmpleoVirutal.Business/LogBl.cs
@@ -0,0 +1,72 @@
+﻿namespace AgenciaDeEmpleoVirutal.Business
+{
+    using AgenciaDeEmpleoVirutal.Business.Referentials;
+    using AgenciaDeEmpleoVirutal.Contracts.Business;
+    using AgenciaDeEmpleoVirutal.Contracts.ExternalServices;
+    using AgenciaDeEmpleoVirutal.Contracts.Referentials;
+    using AgenciaDeEmpleoVirutal.Entities;
+    using AgenciaDeEmpleoVirutal.Entities.ExternalService.Request;
+    using AgenciaDeEmpleoVirutal.Entities.Referentials;
+    using AgenciaDeEmpleoVirutal.Entities.Requests;
+    using AgenciaDeEmpleoVirutal.Utils;
+    using AgenciaDeEmpleoVirutal.Utils.Enum;
+    using AgenciaDeEmpleoVirutal.Utils.Helpers;
+    using AgenciaDeEmpleoVirutal.Utils.Resources;
+    using AgenciaDeEmpleoVirutal.Utils.ResponseMessages;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net.Mail;
+
+    /// <summary>
+    /// Pdi Business Logic
+    /// </summary>
+    public class LogBl : BusinessBase<Log>, ILogBl
+    {
+
+        /// User Repository
+        /// </summary>
+        private IGenericRep<Log> _LogRep;
+
+
+        public LogBl(IGenericRep<Log> LogRep)
+        {
+            _LogRep = LogRep;
+
+        }
+
+        /// <summary>
+        /// Method Create PDI
+        /// </summary>
+        /// <param name="PDIRequest"></param>
+        /// <returns></returns>
+        public Response<Log> SetLog(SetLogRequest logRequest)
+        {
+            var errorsMessage = logRequest.Validate().ToList();
+            if (errorsMessage.Count > 0)
+            {
+                return ResponseBadRequest<Log>(errorsMessage);
+            }
+
+            var log = new Log()
+            {
+                Answered = logRequest.Answered,
+                Caller = logRequest.Caller,
+                Observations = logRequest.Observations,
+                OpenTokAccessToken = logRequest.OpenTokAccessToken,
+                OpenTokSessionId = logRequest.OpenTokSessionId,
+                Type = logRequest.Type,
+                DateLog = DateTime.Now
+            };
+
+            if (!_LogRep.AddOrUpdate(log).Result)
+            {
+                return ResponseFail<Log>();
+            }
+            return ResponseSuccess();
+
+        }
+    }
+}
+
---
+++ AgenciaDeEmpleoVirutal.Entities/Requests/SetLogRequest.cs
@@ -0,0 +1,27 @@
+﻿namespace AgenciaDeEmpleoVirutal.Entities.Requests
+{
+    using AgenciaDeEmpleoVirutal.Entities.Resources;
+    using System.ComponentModel.DataAnnotations;
+
+    public class SetLogRequest
+    {
+        [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "OpenTokSessionId_Required")]
+        public string OpenTokSessionId { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "OpenTokAccessToken_Required")]
+        public string OpenTokAccessToken { get; set; }
+    
+        [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "CallUser_Required")]
+        public string Caller { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "AgentUser_Required")]
+        public string Answered { get; set; }
+
+
+        public string Type { get; set; }
+        public string Observations { get; set; }
+
+
+    }
+
+}
