67e63aa5c11ffec8aa42d33cf3f187fe37b5b4a5
Julian Andres Maldonado Aponte <Jmaldonadoa@intergrupo.com>
2018-12-19 17:17:25 -0500

    code smells.

--- AgenciaDeEmpleoVirutal.Entities/Requests/SetCallTraceRequest.cs
+++ AgenciaDeEmpleoVirutal.Entities/Requests/SetCallTraceRequest.cs
@@ -3,25 +3,44 @@
     using AgenciaDeEmpleoVirutal.Entities.Resources;
     using System.ComponentModel.DataAnnotations;
 
+    /// <summary>
+    /// Set call trace request.
+    /// </summary>
     public class SetCallTraceRequest
     {
-
+        /// <summary>
+        /// Gets or sets the OpenTokSessionId.
+        /// </summary>
         [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "OpenTokSessionId_Required")]
         public string OpenTokSessionId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the OpenTokAccessToken.
+        /// </summary>
         [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "OpenTokAccessToken_Required")]
         public string OpenTokAccessToken { get; set; }
 
-        // username noDoc_codDoc
+        /// <summary>
+        /// Gets or sets the UserName.
+        /// </summary>
         [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "User_Required")]
         public string UserName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the State.
+        /// </summary>
         [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "StateCall_Required")]
         [Range(1, 1000, ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "StateCall_Valid")]
         public int State { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Trace.
+        /// </summary>
         public string Trace { get; set; }
 
+        /// <summary>
+        /// Gets or sets the CallType.
+        /// </summary>
         public string CallType { get; set; }
     }
 }
--- AgenciaDeEmpleoVirutal.Entities/Requests/UpdateFuncionaryRequest.cs
+++ AgenciaDeEmpleoVirutal.Entities/Requests/UpdateFuncionaryRequest.cs
@@ -3,29 +3,56 @@
     using AgenciaDeEmpleoVirutal.Entities.Resources;
     using System.ComponentModel.DataAnnotations;
 
+    /// <summary>
+    /// Update Funcionary Request
+    /// </summary>
     public class UpdateFuncionaryRequest
     {
+        /// <summary>
+        /// Type document.
+        /// </summary>
         [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "TypeId_Required")]
         public string TypeDocument { get; set; }
 
+        /// <summary>
+        /// No document.
+        /// </summary>
         [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "Id_Required")]
         public string NoDocument { get; set; }
 
+        /// <summary>
+        /// Internal mail.
+        /// </summary>
         [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "EmailAddress_Required")]
         public string InternalMail { get; set; }
 
+        /// <summary>
+        /// Position.
+        /// </summary>
         [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "Position_Required")]
         public string Position { get; set; }
 
+        /// <summary>
+        /// Name.
+        /// </summary>
         [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "Name_Required")]
         public string Name { get; set; }
         
+        /// <summary>
+        /// Last name.
+        /// </summary>
         [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "LastName_Required")]
         public string LastName { get; set; }
         
+        /// <summary>
+        /// Role.
+        /// </summary>
         [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "Role_Required")]
         public string Role { get; set; }
 
+        /// <summary>
+        /// State.
+        /// </summary>
         [Required(ErrorMessageResourceType = typeof(EntityMessages), ErrorMessageResourceName = "State_Required")]
         public bool State { get; set; }
     }
