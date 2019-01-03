22ca93efa105f1b89ffa27c7a3c0b9df87be948d
Julian Andres Maldonado Aponte <Jmaldonadoa@intergrupo.com>
2019-01-03 07:49:23 -0500

    Unit test

---
+++ AgenciaDeEmpleoVirutal.UnitedTests/ResetBITest/RegisterResetPasswordTest.cs
@@ -0,0 +1,45 @@
+﻿namespace AgenciaDeEmpleoVirutal.UnitedTests.ResetBITest
+{
+    using AgenciaDeEmpleoVirutal.Entities;
+    using AgenciaDeEmpleoVirutal.Entities.Responses;
+    using AgenciaDeEmpleoVirutal.Utils.ResponseMessages;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class RegisterResetPasswordTest : ResetBITestBase
+    {
+        [TestMethod, TestCategory("ResetBI")]
+        public void RegisterResetPassword_WhenIdIsEmpty_ReturnBadRequest()
+        {
+            /// Arrange
+            var id = string.Empty;
+            var expected = ResponseFail<ResetResponse>(ServiceResponseCode.BadRequest);
+            /// Act
+            var result = resetBusinessLogic.RegisterResetPassword(id);
+            /// Assert
+            Assert.AreEqual(expected.Message.ToString(), result.Message.ToString());
+            Assert.AreEqual(expected.CodeResponse, result.CodeResponse);
+            Assert.IsFalse(result.TransactionMade);
+            Assert.IsNull(result.Data);
+        }
+
+        [TestMethod, TestCategory("ResetBI")]
+        public void RegisterResetPassword_WhenIdUserIsNotFound_ReturnFail()
+        {
+            /// Arrange
+            var id = "10694445";
+            var expected = ResponseFail<ResetResponse>(ServiceResponseCode.UserNotFound);
+            _userRepMock.Setup(u => u.GetAsyncAll(It.IsAny<string>())).Returns(Task.FromResult(new List<User>()));
+            /// Act
+            var result = resetBusinessLogic.RegisterResetPassword(id);
+            /// Assert
+            Assert.AreEqual(expected.Message.ToString(), result.Message.ToString());
+            Assert.AreEqual(expected.CodeResponse, result.CodeResponse);
+            Assert.IsFalse(result.TransactionMade);
+            Assert.IsNull(result.Data);
+        }
+    }
+}
---
+++ AgenciaDeEmpleoVirutal.UnitedTests/ResetBITest/ResetBITestBase.cs
@@ -0,0 +1,53 @@
+﻿namespace AgenciaDeEmpleoVirutal.UnitedTests.ResetBITest
+{
+    using AgenciaDeEmpleoVirutal.Business;
+    using AgenciaDeEmpleoVirutal.Business.Referentials;
+    using AgenciaDeEmpleoVirutal.Contracts.ExternalServices;
+    using AgenciaDeEmpleoVirutal.Contracts.Referentials;
+    using AgenciaDeEmpleoVirutal.Entities;
+    using AgenciaDeEmpleoVirutal.Entities.Responses;
+    using Moq;
+
+    public class ResetBITestBase : BusinessBase<ResetResponse>
+    {
+        /// <summary>
+        /// Interface to dens mails mock.
+        /// </summary>
+        protected Mock<ISendGridExternalService> _sendMailServiceMock;
+
+        /// <summary>
+        /// Interface of Ldap Services mock.
+        /// </summary>
+        protected Mock<ILdapServices> _ldapServicesMock;
+
+        /// <summary>
+        /// User repository mock.
+        /// </summary>
+        protected Mock<IGenericRep<User>> _userRepMock;
+
+        /// <summary>
+        /// Reset Password repository mock.
+        /// </summary>
+        protected Mock<IGenericRep<ResetPassword>> _passwordRepMock;
+
+        /// <summary>
+        /// Parameters repository mock.
+        /// </summary>
+        protected Mock<IGenericRep<Parameters>> _parametersRepMock;
+
+        /// <summary>
+        /// Reset Business Logic
+        /// </summary>
+        protected ResetBI resetBusinessLogic;
+
+        public ResetBITestBase()
+        {
+            _sendMailServiceMock = new Mock<ISendGridExternalService>();
+            _ldapServicesMock = new Mock<ILdapServices>();
+            _userRepMock = new Mock<IGenericRep<User>>();
+            _passwordRepMock = new Mock<IGenericRep<ResetPassword>>();
+            _parametersRepMock = new Mock<IGenericRep<Parameters>>();
+            resetBusinessLogic = new ResetBI(_userRepMock.Object, _passwordRepMock.Object, _parametersRepMock.Object, _sendMailServiceMock.Object, _ldapServicesMock.Object);
+        }
+    }
+}
